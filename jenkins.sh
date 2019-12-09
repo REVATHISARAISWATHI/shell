1cae9771a5bfc0d813cb2637a0e86bb7c3b8a35f
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-09 12:23:18 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,9 +5,9 @@ wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install -y jenkins
 service jenkins start
-sudo iptables -A INPUT -p tcp -m tcp --deport 8080 -j ACCEPT
-sudo service iptables save
-sudo service iptables restart
+#sudo iptables -A INPUT -p tcp -m tcp --deport 8080 -j ACCEPT
+#sudo service iptables save
+#sudo service iptables restart
 
 echo "initialAdminPassword"
 cat /var/lib/jenkins/secrets/initialAdminPassword
