9d20f7ed89cb0f806cf1a887b1c8963b93c638b1
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-09 12:32:40 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,5 +6,7 @@ rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install -y jenkins
 service jenkins start
 
-echo "initialAdminPassword"
+
+
+echo "initial admin password"
 cat /var/lib/jenkins/secrets/initialAdminPassword
